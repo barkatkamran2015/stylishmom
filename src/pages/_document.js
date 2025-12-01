42352859f0468ad74a0562137181fe62a4d59ec5
barkatkamran2015 <71576244+barkatkamran2015@users.noreply.github.com>
2025-11-30 20:30:11 -0800

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* YOUR LOGO IN TABS & GOOGLE SEARCH RESULTS */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon.png" />
+
+        {/* Optional: helps Google pick your logo faster */}
+        <meta name="application-name" content="The Stylish Mama" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
